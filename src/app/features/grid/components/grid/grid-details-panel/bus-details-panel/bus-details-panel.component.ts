f9a5e60be7304d0435a224290161202cbdbca1a1
TudorOrban <tudororban2@gmail.com>
2025-02-25 15:26:53 +0200

    feat(grid-details): Refine Bus Details component

--- src/app/features/grid/components/grid/grid-details-panel/bus-details-panel/bus-details-panel.component.ts
+++ src/app/features/grid/components/grid/grid-details-panel/bus-details-panel/bus-details-panel.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { GridStateService } from '../../../../services/ui/grid-state.service';
 import { CommonModule } from '@angular/common';
-import { LabelValueComponent } from "../../../../../../shared/common/components/label-value/label-value.component";
+import { DataFormatterService } from '../../../../../../shared/common/services/data-formatter.service';
 
 @Component({
   selector: 'app-bus-details-panel',
-  imports: [CommonModule, LabelValueComponent],
+  imports: [CommonModule],
   templateUrl: './bus-details-panel.component.html',
   styleUrl: './bus-details-panel.component.css'
 })
@@ -13,7 +13,31 @@ export class BusDetailsPanelComponent {
 
     constructor(
         readonly gridStateService: GridStateService,
+        readonly dataFormatterService: DataFormatterService
     ) {}
 
+    metadataFields = [
+        { label: "Grid ID", path: "gridId" },
+        { label: "Bus Name", path: "busName" },
+        { label: "Created at", path: "createdAt", type: "date" },
+        { label: "Latitude", path: "latitude" },
+        { label: "Longitude", path: "longitude" },
+    ];
 
+    stateFields = [
+        { label: "Last Updated", path: "state.updatedAt", type: "date" },
+        { label: "Voltage Magnitude", path: "state.voltageMagnitude" },
+        { label: "Voltage Angle", path: "state.voltageAngle" },
+        { label: "Active Power Injection", path: "state.activePowerInjection" },
+        { label: "Reactive Power Injection", path: "state.reactivePowerInjection" },
+        { label: "Phase Shifting Transformer Tap Position", path: "state.phaseShiftingTransformerTapPosition" },
+    ];
+
+    getValue(item: any, path: string, type?: string): string {
+        const value = path.split(".").reduce((acc, part) => acc && acc[part], item);
+        if (type === "date") {
+            return this.dataFormatterService.formatDate(value);
+        }
+        return value ?? "";
+    }
 }
